2cef4710bf8142a13ef2cd0abf285cca7f69d488
Andre Zicatti <andrezicatti@gmail.com>
2025-01-07 18:04:01 -0300

    :recycle: refactor: Categories Paged

--- src/main/java/com/andrezktt/product_catalog/controllers/CategoryController.java
+++ src/main/java/com/andrezktt/product_catalog/controllers/CategoryController.java
@@ -3,6 +3,9 @@ package com.andrezktt.product_catalog.controllers;
 import com.andrezktt.product_catalog.dto.CategoryDTO;
 import com.andrezktt.product_catalog.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -19,8 +22,15 @@ public class CategoryController {
     private CategoryService categoryService;
 
     @GetMapping
-    public ResponseEntity<List<CategoryDTO>> findAll() {
-        return ResponseEntity.ok().body(categoryService.findAll());
+    public ResponseEntity<Page<CategoryDTO>> findAll(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
+            @RequestParam(value = "direction", defaultValue = "ASC") String direction,
+            @RequestParam(value = "orderBy", defaultValue = "name") String orderBy) {
+
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+
+        return ResponseEntity.ok().body(categoryService.findAllPaged(pageRequest));
     }
 
     @GetMapping(value = "/{id}")
